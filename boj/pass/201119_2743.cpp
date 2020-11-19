14b7b73bb5219d4eb0a2570da1276e821393a778
noeykan <noeykan@gmail.com>
2020-11-19 23:41:25 +0900

    단어 길이 재기

---
+++ boj/pass/201119_2743.cpp
@@ -0,0 +1,17 @@
+// 단어 길이 재기
+// https://www.acmicpc.net/problem/2743
+/* 이게 알고리즘 문제라니.. 인생에서 제일 쉬운 문제였음.. */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    cin >> line;
+    cout << line.size();
+
+    return 0;
+}
