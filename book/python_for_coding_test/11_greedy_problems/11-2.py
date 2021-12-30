d012274873aa97e34b465c370211e68a3f383bc8
noeykan <noeykan@gmail.com>
2021-12-31 01:27:10 +0900

    git hook test with discord

--- book/python_for_coding_test/11_greedy_problems/11-2.py
+++ book/python_for_coding_test/11_greedy_problems/11-2.py
@@ -9,4 +9,4 @@ print(result)
 
 # 10분
 # 1. string을 굳이 int list로 바꾸지 않아도 string을 for 문으로 돌면서 int()로 각각 바꿔주면 더 깔끔하다
-# 2. if 문을 result <= 1 or nums <= 1 로 하는 게 더 좋았을 뻔
+# 2. if 문을 result <= 1 or nums <= 1 로 하는 게 더 좋았을 뻔 
