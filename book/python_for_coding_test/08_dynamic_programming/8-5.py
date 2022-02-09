a951f6e1f4da5714edaa32bae9e686201f6d8519
noeykan <noeykan@gmail.com>
2022-02-10 00:05:14 +0900

    ch8-5 add bottom-up solving

--- book/python_for_coding_test/08_dynamic_programming/8-5.py
+++ book/python_for_coding_test/08_dynamic_programming/8-5.py
@@ -1,5 +1,5 @@
 # 2. 1로 만들기
-
+import time
 x = int(input())
 
 # d(1) = 0
@@ -9,21 +9,33 @@ x = int(input())
 # d(5) = 1
 
 # 1. 탑다운(메모이제이션)
-d = [-1] * 30001
-d[1] = 0
-def calc(n):
-    if d[n] != -1:
-        return d[n]
+# d = [-1] * 30001
+# d[1] = 0
+# def calc(n):
+#     if d[n] != -1:
+#         return d[n]
 
-    min_cnt = calc(n - 1)
-    if (n % 5) == 0:
-        min_cnt = min(min_cnt, calc(n // 5))
-    if (n % 3) == 0:
-        min_cnt = min(min_cnt, calc(n // 3))
-    if (n % 2) == 0:
-        min_cnt = min(min_cnt, calc(n // 2))
+#     min_cnt = calc(n - 1)
+#     if n % 5 == 0:
+#         min_cnt = min(min_cnt, calc(n // 5))
+#     if n % 3 == 0:
+#         min_cnt = min(min_cnt, calc(n // 3))
+#     if n % 2 == 0:
+#         min_cnt = min(min_cnt, calc(n // 2))
 
+#     d[n] = min_cnt + 1
+#     return d[n]
+# print(calc(x))
+
+# 2. 바텀업(반복문)
+d = [0] * 30001
+for n in range(2, x + 1):
+    min_cnt = d[n - 1]
+    if n % 5 == 0:
+        min_cnt = min(min_cnt, d[n // 5])
+    if n % 3 == 0:
+        min_cnt = min(min_cnt, d[n // 3])
+    if n % 2 == 0:
+        min_cnt = min(min_cnt, d[n // 2])
     d[n] = min_cnt + 1
-    return d[n]
-
-print(calc(x))
+print(d[x])
