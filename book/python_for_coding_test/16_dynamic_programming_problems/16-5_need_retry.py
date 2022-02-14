8734342fdc0c687b393d2b4cfc642d6559619de3
noeykan <noeykan@gmail.com>
2022-02-15 02:09:05 +0900

    ch16-5 add answer

--- book/python_for_coding_test/16_dynamic_programming_problems/16-5_need_retry.py
+++ book/python_for_coding_test/16_dynamic_programming_problems/16-5_need_retry.py
@@ -16,6 +16,27 @@ for i in range(1, N):
 print(d[-1])
 
 # 풀긴 풀었으나 시간 복잡도가 O(n^2) 라서 내 알고리즘은 별로임
-# 아래 답지 알고리즘을 이해하고 나중에 다시 풀어보자
+# 아래 답지 알고리즘을 이해하고 나중에 다시 풀어보자.. 이런 생각을 어떻게 하지
 
-# 답지풀이: O(n)
+# # 답지풀이: O(n)
+# d = [0] * N
+# d[0] = 1
+
+# i2 = i3 = i5 = 0
+# next2 = 2
+# next3 = 3
+# next5 = 5
+
+# for i in range(1, N):
+#     d[i] = min(next2, next3, next5)
+#     if d[i] == next2:
+#         i2 += 1
+#         next2 = d[i2] * 2
+#     if d[i] == next3:
+#         i3 += 1
+#         next3 = d[i3] * 3
+#     if d[i] == next5:
+#         i5 += 1
+#         next5 = d[i5] * 5
+# print(d)
+
